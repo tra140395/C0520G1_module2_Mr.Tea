b72a0b9b4c8fc0b507c0a5db1a30325e474886a0
tra140395 <sontra140395@gmail.com>
2020-07-13 01:01:01 +0700

    nop bai

---
+++ b11stackandqueue/ThucHanh/Node.java
@@ -0,0 +1,11 @@
+package b11stackandqueue.ThucHanh;
+
+public class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}
